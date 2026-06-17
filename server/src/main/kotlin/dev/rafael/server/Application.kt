1c6c49a8a1ca1a8344743bf64a55d736b2cac408
Rafa Dev <rafel0017@gmail.com>
2026-06-16 22:20:57 -0300

    test: viola fronteira p/ provar que o CI barra (REVERTER)

--- server/src/main/kotlin/dev/rafael/server/Application.kt
+++ server/src/main/kotlin/dev/rafael/server/Application.kt
@@ -9,6 +9,7 @@ import dev.rafael.server.error.toHttp
 import io.ktor.serialization.kotlinx.json.json
 import io.ktor.server.application.Application
 import io.ktor.server.application.install
+import androidx.annotation.Nullable
 import io.ktor.server.netty.EngineMain
 import io.ktor.server.plugins.calllogging.CallLogging
 import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
